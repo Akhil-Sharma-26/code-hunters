7ac0020dddf3c1fa1a25fad13a655b1566fd1bf2
Ank206 <akapoor1_be22@thapar.edu>
2023-06-02 18:44:15 +0530

    HomePage

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -12,6 +12,12 @@ const Home = () => {
     </div>
     <div className="home2">
       <img src={vg} alt="Vector" />
+      <div>
+      <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, accusamus asperiores perferendis facilis, distinctio hic animi natus error tempora, officiis facere quod sapiente a odio magnam minima dolorem pariatur amet excepturi voluptates quo modi expedita incidunt? Quos tenetur veniam nostrum eum numquam rerum! Inventore quis, id iure modi quasi quam, dolorem, animi iste itaque enim perspiciatis et? Nesciunt est optio aspernatur similique accusantium repudiandae beatae vero iure magnam fugiat dicta enim laborum quisquam numquam, molestiae dolor quo, autem quia unde! Perspiciatis laudantium architecto vitae impedit provident sit exercitationem magni natus doloremque, iusto libero numquam recusandae odio deserunt dolorem suscipit sed!</p>
+      </div>
+    </div>
+    <div className="home3">
+      <div></div>
     </div>
     </>
   )
