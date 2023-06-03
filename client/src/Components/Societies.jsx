90ec40ab6461b11307d079bfcdafda34e16e6dc5
Akhil-Sharma-26 <asharma1_be22@thapar.edu>
2023-06-03 16:02:32 +0530

    created_societies

--- client/src/Components/Societies.jsx
+++ client/src/Components/Societies.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import "../Styles/Societies.scss"
+// import "../Styles/Societies.scss"
+import "../Styles/Societies.css"
+import Header from "./Header"
 const Societies = () => {
     return (
       <>  
@@ -7,22 +9,28 @@ const Societies = () => {
           <h1>Societies</h1>
           <div className="cards">
               <div className="c1">
-                  <div className="pic"></div>
+                  <div className="pic">
+                    <img src="../Assets/ccs_logo.png" alt="ccs" />
+                  </div>
                   {/* <hr/> */}
-                  <h3>Society1</h3>
+                  <h3>Creative Coding Society</h3>
               </div>
               <div className="c1">
-                  <div className="pic">Picture</div>
-                  <h3>Society2</h3>
+                  <div className="pic">
+                    <img src="../Assets/enactus_logog.png" alt="enactus" />
+                  </div>
+                  <h3>ENACTUS</h3>
               </div>
               <div className="c1">
-                  <div className="pic">Picture</div>
-                  <h3>Society3</h3>
+                  <div className="pic">
+                    <img src="../Assets/taas_logo.png" alt="taas" className=''/>
+                  </div>
+                  <h3>Thapar Amatuare Astronomical Society</h3>
               </div>
           </div>
       </div>
       </>
     )
   }
-  
+  <Header/>
   export default Societies
