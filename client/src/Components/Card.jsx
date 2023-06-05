e9be6b09ab828af303ebfa0888a959f885633b5a
Ank206 <akapoor1_be22@thapar.edu>
2023-06-05 20:26:04 +0530

    New maping of Cards

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -7,7 +7,7 @@ const Card = (props) => {
     <div className="body">
         <div className="logo"></div>
         <h3 className="name">{props.name}</h3>
-        <p className="description">Creative computing Society is back with HackTU, flagship event to be hosted at Thapar Institute of Engineering and Technology, Patiala and we're excited to see what new and exciting ideas will come out of this year's event.</p>
+        <p className="description">{props.desc}</p>
     </div>
     </>
   )
