6e1adc7b9923a6985fa89de8bf891a4f5852ae03
Anush Kapoor <121352558+Ank206@users.noreply.github.com>
2023-12-23 16:26:44 +0530

    Clearence Commit

--- client/app.js
+++ client/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 // ! MiddleWare
+
+const DBConnection = "";
+mongoose
+  .connect(DBConnection, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => console.log(`Failed to connect to DB. Error: ${err}`));
 
 const middleWare = (req, res, next) => {
   console.log("Hello it's MiddleWare");
@@ -9,7 +23,7 @@ const middleWare = (req, res, next) => {
 };
 
 app.get("/", (req, res) => {
-  res.send(`${all}`);
+  res.send(`Hi, this is Home Page.`);
 });
 
 app.get("/about", middleWare, (req, res) => {
@@ -17,7 +31,7 @@ app.get("/about", middleWare, (req, res) => {
 });
 
 app.listen(8000, () => {
-  console.log("Listening");
+  console.log("Listening...");
 });
 
-console.log("Hi");
+console.log("Server Started.");
