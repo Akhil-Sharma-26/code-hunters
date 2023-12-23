13db9d1210c0be17a940d42050a5b7baceba874f
Anush Kapoor <121352558+Ank206@users.noreply.github.com>
2023-12-24 01:29:37 +0530

    Setting Up the Proxy for the CORS left, in response.

--- client/app.js
+++ client/app.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT;
 
 require("./db");
 
-// Linking Router Files
 app.use(express.json());
 // above one to convert the data into the JSON format.
+// Linking Router Files
 app.use(require("./router/auth"));
 
 app.listen(PORT, () => {
