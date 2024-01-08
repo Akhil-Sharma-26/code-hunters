0115c42132dad096069c0296c67a6b94e27fc26f
Anush Kapoor <121352558+Ank206@users.noreply.github.com>
2024-01-08 21:25:39 +0530

    Deployment-2 : Hopefully Final

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,26 +1,2 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const app = express();
-const mongoose = require("mongoose");
-// const cors = require("cors");
-
-// ! MiddleWare
-
-dotenv.config({ path: "./.env" });
-
-const DBConnection = process.env.MONG_URL;
-const PORT = process.env.PORT || 5000;
-require("./db");
-
-// app.use(cors(corsOptions));
-
-app.use(express.json());
-// above one to convert the data into the JSON format.
-// Linking Router Files
-app.use(require("./router/auth"));
-
-app.listen(PORT, () => {
-  console.log(`Listening to port:${PORT}...`);
-});
-
-console.log("Server Started.");
+import app from "../app";
+export default app;
